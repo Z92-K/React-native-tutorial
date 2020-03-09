37845a8b16548396aeb37813268a95eb9e2ada44
Zanele Kukuma <zanelezanzizo@gmail.com>
2020-03-09 16:38:24 +0200

    Pick an image from files and display it on the app

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text,TouchableOpacity, View } from 'react-native';
 import logo from './assets/zan.jpg'; 
+import * as ImagePicker from 'expo-image-picker';
+
 
 export default function App() {
+  let [selectedImage, setSelectedImage] = React.useState(null);
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  
+  }
   return (
     <View style={styles.container}>
       <Image source={logo} style={{ width: 305, height: 159 }} />
-      <Text style={{color: '#888', fontSize: 18}} >Why doesn't it work!?</Text>
+      <Text style={{color: '#888', fontSize: 18}} > Why doesn't it work!?</Text>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}  style={styles.button}>
+        <Text style={styles.buttonText}> Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +51,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   },
 });
